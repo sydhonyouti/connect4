d1cc806941175ebd98ae0f4a075483916bc50f5b
Sydney H <64348633+sydhonyouti@users.noreply.github.com>
2022-12-04 21:17:57 -0600

    Created a board layout for game. Needs fixing still

--- connect4Game.py
+++ connect4Game.py
@@ -1,2 +1,30 @@
 # Main file for the game implementation
 # Need to add pygame and numpy to use the user interaction for connect 4
+
+# Global Variables
+row = 6
+col = 7
+board = []
+boardLabel = []
+winner = False
+
+# Board setup
+def createBoard(row, col):
+    for i in range(8):
+        if i < row:
+            board.append(["_ " * col])
+        elif i < col:
+            board.append(["^"] * col)
+        else:
+            for j in range(row):
+                boardLabel.append(str(j + 1))
+            board.append(boardLabel)
+
+def printBoard(board):
+    print("\n")
+    for row in board:
+        print(" ".join(row))
+    print("\n")
+
+createBoard(row,col)
+printBoard(board)
