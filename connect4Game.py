47bd734bf9b8eb46a396cad88a59201571a12c73
Sydney H <64348633+sydhonyouti@users.noreply.github.com>
2022-12-05 21:14:54 -0600

    Added play game function. Haven't checked it because I need to make a main game function loop to check it

--- connect4Game.py
+++ connect4Game.py
@@ -7,6 +7,9 @@ col = 7
 board = []
 boardLabel = []
 winner = False
+lastTurn = 0
+AI = False
+Player = True
 
 # Board setup
 def createBoard(row, col):
@@ -25,6 +28,33 @@ def printBoard(board):
     for row in board:
         print(" ".join(row))
     print("\n")
+
+
+# Putting board pieces down
+def dropPiece(lastTurn, col):
+    if Player == True:
+        board[lastTurn][col] = "X"
+    else:
+        board[lastTurn][col] = "O"
+    printBoard(board)
+
+# Keep the game to running (True) til it breaks. Then we can stop the game
+def playGame():
+    while True:
+        try:
+            column = int(input("Pick a column (1 - 7)")) - 1
+            if column >= 1 and column <= col:
+                for i in range(6):
+                    if board[i][column] == "_":
+                        last = i
+                dropPiece(last, column)
+            else:
+                raise "ERROR: You picked a column outside the board"
+            break
+        except:
+            print("Not a valid number! Please try again")
+
+# Function to check for a player win
 
 createBoard(row,col)
 printBoard(board)
